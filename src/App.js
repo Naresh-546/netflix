9899d79ab95ee7efa39fd4cecababb54454a0ff4
Naresh Choudhari <599choudhari@gmail.com>
2021-08-11 16:19:17 +0530

    Finally Updated

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow={true}></Row>
       
-      <Row title="TOp Rated" fetchUrl={requests.fetchTopRated}></Row>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}></Row>
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}></Row>
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}></Row>
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}></Row>
